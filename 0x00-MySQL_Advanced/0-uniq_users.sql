8c7300861e137bff78606492352daf699a376d31
AnasJafir <dr.anas.jafir@gmail.com>
2024-07-29 19:05:09 +0100

    Backend Storage

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,13 +1,6 @@
--- SQL script that creates a table users following these requirements:
--- With these attributes:
--- id, integer, never null, auto increment and primary key
--- email, string (255 characters), never null and unique
--- name, string (255 characters)
--- If the table already exists, your script should not fail
--- The script can be executed on any database
-
-CREATE DATABASE IF NOT EXISTS holberton;
-CREATE TABLE IF NOT EXISTS users (
+-- Creates a table with unique users.
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,11 +1,16 @@
--- Write a SQL script that creates a trigger that resets the attribute valid_email only when the email has been changed.
-DELiMITER $$
-CREATE TRIGGER reset_valid_email
+-- Creates a trigger that resets the attribute valid_email
+-- only when the email has been changed.
+-- @author Bezaleel Olakunori <https://github.com/B3zaleel>
+DROP TRIGGER IF EXISTS validate_email;
+DELIMITER $$
+CREATE TRIGGER validate_email
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF NEW.email <> OLD.email THEN
+    IF OLD.email != NEW.email THEN
         SET NEW.valid_email = 0;
+    ELSE
+        SET NEW.valid_email = NEW.valid_email;
     END IF;
-END; $$
+END $$
 DELIMITER ;
